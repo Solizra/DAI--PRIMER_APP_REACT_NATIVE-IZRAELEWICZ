c36788dda8b5baf8f85b5563c171ac2eff64e6ba
SOL DIANA IZRAELEWICZ <48796333@ort>
2025-05-05 11:13:23 -0300

    Finalizado

--- Primer-Proyecto/App.js
+++ Primer-Proyecto/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 
 export default function App() {
   const [contador, setContador] = useState(0);
@@ -10,17 +10,15 @@ export default function App() {
 
   return (
     <ImageBackground
-      source={{ uri: 'https://fondosmil.co/fondo/54342.jpg' }} // Fondo con una imagen
+      source={{ uri: 'https://i.pinimg.com/736x/04/35/ca/0435cad177fbb6224844f95bbf1ff855.jpg' }} 
       style={styles.background}
     >
       <View style={styles.container}>
-        <Text style={styles.titulo}>Bienvenido a mi contador</Text>
 
+        <Text style={styles.titulo}>MI CONTADOR</Text>
         <Text style={styles.contadorTexto}>Contador: {contador}</Text>
+        <Button title="Aumentar" onPress={aumentarContador} color="#43AAF1" />
 
-        <TouchableOpacity style={styles.boton} onPress={aumentarContador}>
-          <Text style={styles.botonTexto}>Aumentar +1</Text>
-        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
@@ -36,7 +34,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
-    backgroundColor: 'rgba(0,0,0,0.4)', // Le da un fondo semitransparente al contenido
   },
   titulo: {
     fontSize: 30,
@@ -48,16 +45,5 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: '#fff',
     marginBottom: 30,
-  },
-  boton: {
-    backgroundColor: '#00BFFF',
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    borderRadius: 10,
-  },
-  botonTexto: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: 'bold',
   },
 });
